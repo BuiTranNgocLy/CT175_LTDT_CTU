522ba6221475eb13fc247ca46d28a19bd15a2b0a
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-08-12 21:08:56 +0700

    Update 4_2.c

--- CT175/TH_else_VTTHUC/Khoi_dong/4_2.c
+++ CT175/TH_else_VTTHUC/Khoi_dong/4_2.c
@@ -1,23 +1,18 @@
-#include<conio.h>
-#include<stdio.h>
-int main()
-{
-    int n, i,k; 
-//    printf("Nhap n: ");
-    scanf("%d",&n);
-    for(;n<=0;)
-    {
-        printf("Phai nhap n > 0 \nNhap lai n: ");
-        scanf("%d",&n);
-    }
-    // Kiem tra dieu kien cua n
-    for(i = 1; i <= n; i++)
-    {
-        for(k = 1; k <= i; k++)
-        {
-            printf("* ");            
-        }
-        printf("\n");
-    }    
-    // In ra tam giac vuong co chieu cao n
+#include <stdio.h>
+int main(){
+    int h, i, j  ;
+    scanf("%d",&h);
+    for(i = 1; i <= h; i++) {
+        for(j = 1; j <= h; j++) {
+            if(j<=h-i){
+            	printf(" "); 
+			}
+			else printf("*"); 
+	   }
+		printf("\n");
+   }
+   return 0; 
 }
+
+
+
