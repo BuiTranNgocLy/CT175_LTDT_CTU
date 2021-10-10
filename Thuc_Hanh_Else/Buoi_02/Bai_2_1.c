2ad7e65a6de0ac5fee8724d9efb3b9feada45d3c
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 21:41:53 +0700

    Update Bai_2_1.c

--- Thuc_Hanh_Else/Buoi_02/Bai_2_1.c
+++ Thuc_Hanh_Else/Buoi_02/Bai_2_1.c
@@ -1,3 +1,69 @@
+/*Cho một đồ thị vô hướng đơn. Hãy dựng (các) cây DUYỆT ĐỒ THỊ khi duyệt đồ thị theo chiều rộng bắt đầu từ đỉnh 1.
+
+Nếu đồ thị không liên thông, sau khi duyệt xong lần 1, tìm đỉnh có chỉ số nhỏ nhất chưa duyệt mà duyệt nó, và cứ tiếp tục như thế cho đến khi tất cả các đỉnh đều được duyệt.
+
+Quy ước:
+
+Các đỉnh kề của 1 đỉnh được liệt kê theo thứ tự tăng dần
+
+
+Đầu vào (Input):
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đỉnh và số cung.
+
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng mô tả cung (u, v).
+
+Đầu ra (Output):
+
+In ra cây duyệt đồ thị theo định dạng:
+
+1 <đỉnh cha của 1>
+2 <đỉnh cha của 2>
+....
+i <đỉnh cha của i>
+...
+n <đỉnh cha của n>
+
+Quy ước: Nếu 1 đỉnh không có đỉnh cha (nó là đỉnh gốc của cây) thì đỉnh cha của nó là 0.
+
+Xem thêm các ví dụ bên dưới.
+
+
+
+Gợi ý:
+
+Sử dụng mảng parent[u] để lưu đỉnh cha của đỉnh u.
+Trong quá trình duyệt, thay vì in các đỉnh ra màn hình, ghi nhận lại đỉnh cha của các đỉnh.
+Khi duyệt xong lần lượt in ra u và parent[u] (u chạy từ 1 đến n).
+
+
+Hướng dẫn đọc dữ liệu và chạy thử chương trình
+Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
+Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
+Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
+	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
+	Graph G;
+	int n, m, u, v, w, e;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	for (e = 0; e < m; e++) {
+		scanf("%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+Input
+4 3
+2 1
+1 3
+2 4
+Result
+1 0
+2 1
+3 1
+4 2*/
+
+
 #include<stdio.h>
 #define MAX_Vertices 20
 #define MAX_Length 20
