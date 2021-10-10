a703aeb9717c158cb1a3a1a93ef6a76c481b5bf9
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 17:33:18 +0700

    Update Bai_3.c

--- Thuc_Hanh_Else/Buoi_01/Bai_3.c
+++ Thuc_Hanh_Else/Buoi_01/Bai_3.c
@@ -1,3 +1,15 @@
+/* Cho cấu trúc dữ liệu đồ thị được khai báo sử dụng ma trận đỉnh - đỉnh như sau:
+
+typedef struct {
+    int A[100][100];
+    int n;
+} Graph;
+Giả sử đồ thị vô hướng, không chứa khuyên, không chứa đa cung
+
+Viết hàm int deg(Graph* G, int x) để tính bậc của đỉnh x.
+
+int deg(Graph* G, int x) {
+}*/
 int deg(Graph *G, int x){
     int deg=0, i;
     for(i=0; i<=G->n;i++){
