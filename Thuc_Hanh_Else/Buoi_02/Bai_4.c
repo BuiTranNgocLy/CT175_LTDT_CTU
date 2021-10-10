9e2a4fc26a4596706d60b217a07caac4f43c233a
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 21:36:01 +0700

    Update Bai_4.c

--- Thuc_Hanh_Else/Buoi_02/Bai_4.c
+++ Thuc_Hanh_Else/Buoi_02/Bai_4.c
@@ -1,3 +1,23 @@
+/*Tôn Ngộ Không là một trong các nhân vật chính của truyện Tây du ký. Khi còn ở Hoa Quả Sơn, Tôn Ngộ Không là vua của loài khỉ. Hoa Quả Sơn có rất nhiều cây ăn trái, nên loài khỉ rất thích. Do đặc tính của mình, khỉ không thích đi bộ mà chỉ thích chuyền từ cây này sang một cây khác. Tuy nhiên, nếu khoảng cách giữa hai cây quá xa thi chúng không thể chuyền qua lại được.
+
+Đường đường là vua của loài khỉ, Tôn Ngộ Không muốn vạch ra một kế hoạch hái trái cây trên tất cả các cây có trên Hoa Quả Sơn mà không cần phải nhảy xuống đất. Tôn Ngộ Không dự định sẽ bắt đầu leo lên một cây, hái trái của cây này, sau đó chuyền qua một cây kế tiếp hái trái của này và tiếp tục như thế cho đến khi tất cả các cây đều được hái trái. Một cây có thể được chuyền qua chuyền lại nhiều lần.
+
+Hãy giúp Tôn Ngộ Không kiểm tra xem kế hoạch này có thể thực hiện được không.
+
+Đầu vào (Input):
+Giả sử số lượng cây ăn trái ở Hoa Quả Sơn là n cây và được đánh số từ 1 đến n.
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số cây và số cặp cây có thể chuyền qua lại.
+
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng có thể chuyền từ cây u sang cây v hoặc chuyền từ cây v sang cây u.
+
+Đầu ra (Output):
+
+Nếu kế hoạch của Tôn Ngộ Không có thể thực hiện được DUOC, ngược lại in ra KHONG.
+
+Xem thêm ví dụ bên dưới. Trong ví dụ đầu tiên, Tôn Ngộ Không bắt đầu từ cây 1, chuyền qua cây 2, sau đó chuyền ngược về 1, chuyền tiếp sang 3 và sau cùng là sang 4.*/
 #include<stdio.h>
 #define MAX_Vertices 20
 #define MAX_Length 20
