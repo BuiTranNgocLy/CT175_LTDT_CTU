ccc3ab8e67f4ba384a20658a526f5f41cc27025c
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 23:07:08 +0700

    Update Bai_4.c

--- Thuc_Hanh_Else/Buoi_03/Bai_4.c
+++ Thuc_Hanh_Else/Buoi_03/Bai_4.c
@@ -1,3 +1,48 @@
+/*Đất nước CyberGraph có n thành phố và m con đường. Mỗi con đường nối 2 thành phố lại với nhau. tất cả các con đường đều là đường 2 chiều, mỗi con đường có một chiều dài nào đó. giữa hai thành phố có nhiều nhất là 1 con đường.
+
+Tổng thổng của nước này dự định sẽ đi từ thành phố s đến thành phố t. Đương nhiên, ông ta sẽ chọn hành trình có tổng chiều dài các con đường đi qua ngắn nhất.
+
+Hãy giúp Ngài tổng thổng tìm hành trình ngắn nhất đi từ s đến t.
+
+Đầu vào (Input):
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số thành phố và số con đường.
+
+- m dòng tiếp theo mỗi dòng chứa 3 số nguyên u v d mô tả con đường nối hai thành phố u và v có chiều dài d.
+
+- Dòng cuối cùng chứa số nguyên s t là thành phố bắt đầu và kết thúc.
+
+Đầu ra (Output):
+
+In ra màn hình tổng chiều dài các con đường của hành trình ngắn nhất..
+
+Xem thêm ví dụ bên dưới.
+
+For example:
+
+Input	Result
+4 3	12
+1 2 5
+2 3 2
+3 4 5
+1 4
+
+4 3	7
+1 2 3
+2 4 4
+1 4 10
+1 4
+
+4 4	8
+1 2 2
+2 4 4
+1 3 6
+1 4 10
+2 3
+*/
+
 #include<stdio.h>
 #define MAX_Vertices 100 //dinh
 #define MAX_Edges 500 //cung
