85e6d7a135deed58f0a9a657eff2a63511c2ef26
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 21:36:51 +0700

    Update Bai_3.c

--- Thuc_Hanh_Else/Buoi_02/Bai_3.c
+++ Thuc_Hanh_Else/Buoi_02/Bai_3.c
@@ -1,3 +1,35 @@
+/*Có n hòn đảo và m cây cầu. Mỗi cây cầu bắt qua 2 hòn đảo. Một hôm chúa đảo tự hỏi là với các cây cầu hiện tại thì đứng ở một hòn đảo bất kỳ có thể nào đi đến được tất cả các hòn đảo khác hay không.
+
+Hãy giúp chúa đảo viết chương trình kiểm tra.
+
+Đầu vào (Input):
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đảo và số cây cầu.
+
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng có 1 cây cầu bắt qua hai hòn đảo u và v.
+
+Đầu ra (Output):
+
+Nếu có thể đi được in ra màn hình YES, ngược lại in ra NO.
+
+Xem thêm ví dụ bên dưới.
+
+For example:
+
+Input	Result
+4 3
+1 2
+2 3
+3 4
+YES
+4 2
+3 4
+1 2
+NO*/
+
+
 #include<stdio.h>
 #define MAX_Vertices 20
 #define MAX_Length 20
