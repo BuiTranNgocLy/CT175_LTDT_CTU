3a99fbd2d21993444e9d8f16ab2971b71c7c8924
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 21:35:10 +0700

    Update Bai_5.c

--- Thuc_Hanh_Else/Buoi_02/Bai_5.c
+++ Thuc_Hanh_Else/Buoi_02/Bai_5.c
@@ -1,3 +1,38 @@
+/*Cho G=<V, E> là một đồ thị vô hướng đơn (không có khuyên, không có đa cung). Hãy viết chương trình kiểm tra xem có chứa chu trình hay không.
+
+Chu trình là một đường đi đơn cung có đỉnh đầu trùng với đỉnh cuối.
+
+Đầu vào (Input):
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đỉnh và số cung.
+
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u, v mô tả cung (u, v).
+
+Đầu ra (Output):
+
+In ra màn hình YES nếu đồ thị có chứa chu trình, ngược lại in ra NO
+
+Xem thêm ví dụ bên dưới.
+
+
+
+Hướng dẫn đọc dữ liệu và chạy thử chương trình
+Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
+Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
+Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
+	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
+	Graph G;
+	int n, m, u, v, w, e;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	for (e = 0; e < m; e++) {
+		scanf("%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+*/
 #include<stdio.h>
 #define MAX_Vertices 100  
 #define MAX_Length 20
