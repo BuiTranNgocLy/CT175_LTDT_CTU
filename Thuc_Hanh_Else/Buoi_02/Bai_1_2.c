4dea96377099f81d195b80496d3475b9eff4f3c8
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-10-10 21:45:50 +0700

    Update Bai_1_2.c

--- Thuc_Hanh_Else/Buoi_02/Bai_1_2.c
+++ Thuc_Hanh_Else/Buoi_02/Bai_1_2.c
@@ -1,3 +1,43 @@
+/*Cho một đồ thị vô hướng đơn. Hãy in ra thứ tự của các đỉnh khi duyệt đồ thị theo chiều sâu (sử dụng ĐỆ QUY) bắt đầu từ đỉnh 1. Nếu đồ thị không liên thông,
+sau khi duyệt xong lần 1, tìm đỉnh có chỉ số nhỏ nhất chưa duyệt mà duyệt nó, và cứ tiếp tục như thế cho đến khi tất cả các đỉnh đều được duyệt.
+Quy ước:
+Các đỉnh kề của 1 đỉnh được liệt kê theo thứ tự tăng dần.
+
+Đầu vào (Input):
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đỉnh và số cung.
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng mô tả cung (u, v).
+
+Đầu ra (Output):
+- In ra các đỉnh theo thứ tự duyệt, mỗi đỉnh trên 1 dòng.
+Xem thêm các ví dụ bên dưới.
+
+Hướng dẫn đọc dữ liệu và chạy thử chương trình
+Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
+Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
+Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
+	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
+	Graph G;
+	int n, m, u, v, w, e;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	for (e = 0; e < m; e++) {
+		scanf("%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+Input
+4 3
+2 1
+1 3
+2 4
+Result
+1
+2
+4
+3 */
+
+
 #include<stdio.h>
 #define MAX_Vertices 20
 #define MAX_Length 20
