6cc0caafc4a1da34677e65087c979909bab80e2a
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-11-10 20:15:50 +0700

    Update Bai_2_Can_Da.c

--- Thuc_Hanh_Else/Buoi_04/Bai_2_Can_Da.c
+++ Thuc_Hanh_Else/Buoi_04/Bai_2_Can_Da.c
@@ -1,3 +1,36 @@
+/*
+Peter rất thích chơi đá. Anh ta thường dùng đá để trang trí sân nhà của mình. Hiện tại Peter có n hòn đá. Dĩ nhiên mỗi hòn đá có một khối lượng nào đó. Peter muốn đặt các hòn đá này dọc theo lối đi từ cổng vào nhà của mình. Peter lại muốn sắp xếp như thế này: hòn đá nặng nhất sẽ đặt ở cạnh cổng rào, kế tiếp là hòn đá nặng thứ 2, ... hòn đá nhẹ nhất sẽ được đặt cạnh nhà. Như vậy nếu đi từ trong nhà ra cổng, ta sẽ gặp các hòn đá có khối lượng tăng dần.
+Tuy nhiên, điều khó khăn đối với Peter là anh chỉ có một cây cân đĩa mà không có quả cân nào. Nói cách khác, mỗi lần cân Peter chỉ có thể biết được hòn đá nào nhẹ hơn hòn đá nào chứ không biết nó nặng bao nhiêu kg.
+Sau m lần cân, Peter biết được sự khác nhau về cân nặng của m cặp. Với các thông tin này, hãy giúp Peter sắp xếp các viên đá theo thứ tự anh mong muốn.
+Đầu vào (Input):
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số hòn đó và số lần cân
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u v nói rằng hòn đá u nhẹ hơn hòn đá v.
+Đầu ra (Output):
+In ra màn hình thứ tự của các hòn đá theo khối lượng tăng dần. In các số thứ tự trên cùng một dòng, mỗi số cách nhau một khoảng trắng.
+Bạn có thể yên tâm là dữ liệu đầu được giả sử rằng chỉ có một kết quả quả duy nhất.
+Xem thêm ví dụ bên dưới. Trong ví dụ đầu tiên ta có: hòn đá 1 nhẹ nhất, kế đến là hòn đá 3 và sau cùng là hòn đá 2.
+For example:
+Input	  Result
+3 2     1
+1 3     3
+3 2     2
+
+7 13    1
+1 2     2
+1 3     4
+1 4     6
+2 3     5
+2 6     3
+3 7     7
+4 5
+5 3
+5 7
+6 7
+2 4
+6 5
+4 6
+*/
 #include <stdio.h>
 #define MAX_Vertices 100
 #define MAX_Length 20
