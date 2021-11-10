99bcd4c0802f18320d853dfef25276a6a2904981
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-11-10 20:10:23 +0700

    Update Bai_1.c

--- Thuc_Hanh_Else/Buoi_04/Bai_1.c
+++ Thuc_Hanh_Else/Buoi_04/Bai_1.c
@@ -1,3 +1,38 @@
+/*Viết chương trình xếp hạng cho đồ thị có hướng không chu trình.
+Đầu vào (Input):
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số đỉnh và số cung.
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên u, v mô tả cung (u, v).
+
+Đầu ra (Output):
+In ra màn hình hạng của các đỉnh theo thứ tự của đỉnh, mỗi đỉnh trên 1 dòng:
+Hạng đỉnh 1
+Hạng đỉnh 2
+...
+Hạng đỉnh n
+Xem thêm ví dụ bên dưới. Trong ví dụ đầu tiên ta có: hạng của 1 = 0, hạng của 2 = 2 và hạng của 3 = 1.
+
+Chú ý:
+Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
+Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
+Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
+	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
+	Graph G;
+	int n, m, u, v, w, e;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	for (e = 0; e < m; e++) {
+		scanf("%d%d%d", &u, &v);
+		add_edge(&G, u, v);
+	}
+For example:
+
+Input	Result
+3 2  	
+1 3
+3 2
+*/
 #include <stdio.h>
 #define MAX_Vertices 100
 #define MAX_Length 20
