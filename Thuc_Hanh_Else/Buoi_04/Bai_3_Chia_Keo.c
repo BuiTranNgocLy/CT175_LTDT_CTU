6d24a466bd46613b3f2ef6b9a77767809a37480c
Bùi Trần Ngọc Ly <88178841+BuiTranNgocLy@users.noreply.github.com>
2021-11-10 20:16:31 +0700

    Update Bai_3_Chia_Keo.c

--- Thuc_Hanh_Else/Buoi_04/Bai_3_Chia_Keo.c
+++ Thuc_Hanh_Else/Buoi_04/Bai_3_Chia_Keo.c
@@ -1,3 +1,47 @@
+/*
+Cô giáo Trang chuẩn bị kẹo để phát cho các bé mà cô đang giữ. Dĩ nhiên môi bé đều có một tên gọi rất dễ thương ví dụ: Mạnh Phát, Diễm Quỳnh, Đăng Khoa, ... Tuy nhiên, để đơn giản vấn đề ta có thể giả sử các em được đánh số từ 1 đến n.
+Cô giáo muốn rằng tất cả các em đều phải có kẹo. Cô lại biết thêm rằng có một số bé có ý muốn hơn bạn mình một chút vì thế các em ấy muốn kẹo của mình nhiều hơn của bạn.
+Hãy viết chương trình giúp cô tính xem mỗi em cần được chia ít nhất bao nhiêu kẹo và tổng số kẹo ít nhất mà cô phải chuẩn bị là bao nhiêu.
+
+Đầu vào (Input):
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m, tương ứng là số bé và số cặp bé mà trong đó có 1 bé muốn có kẹo hơn bạn mình..
+
+- m dòng tiếp theo mỗi dòng chứa 2 số nguyên a, b nói rằng bé a muốn có kẹo nhiều hơn bé b.
+
+Đầu ra (Output):
+
+In ra màn hình số kẹo ít nhất của từng em, mỗi em trên một dòng.
+
+Dòng cuối cùng in tổng số kẹo ít nhất mà cô giáo Trang cần phải chuẩn bị
+
+Chú ý:
+
+Xem thêm các ví dụ để hiểu thêm về đầu vào và đầu ra.
+
+For example:
+
+Input	  Result
+3 2     3
+1 3     1
+3 2     2
+        6
+        
+7 10    1
+2 1     2
+3 1     4
+4 1     2
+3 2     3
+6 2     3
+7 3     3
+5 4     5
+3 5     20
+7 5 
+7 6
+
+*/
 #include <stdio.h>
 #define MAX_Vertices 100
 #define MAX_Length 20
